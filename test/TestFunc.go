9fe683c1a664414f157170564c51aa6639214975
zhaoxi <fly0307@sjtu.edu.cn>
2023-05-31 19:38:19 +0800

    modify FuncTest

--- test/TestFunc.go
+++ test/TestFunc.go
@@ -1,10 +1,10 @@
 package main
 
 func main() {
-	//CreatePod()
+	CreatePod()
 	//GetResourseByHTTP()
 	//GetResourseTest()
 	//query := "sum(container_cpu_usage_seconds_total{name=\"autoscale\"})"
 	//PromtheusQuery(query)
-	GetMetricsTest()
+	//GetMetricsTest()
 }
